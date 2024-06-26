5f268b4cb7b9589052867d023e37ecb440339863
zBragaTo <leonardobragatobassi@hotmail.com>
2024-06-26 20:28:05 -0300

    With Extends

--- src/CuringaDel/Program.java
+++ src/CuringaDel/Program.java
@@ -10,11 +10,15 @@ public class Program {
 		List<Shape> myShapes = new ArrayList<>();
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
+		
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(3.0));
+		myCircles.add(new Circle(2.0));
 		 
-		System.out.println("Total area: " + totalArea(myShapes));
+		System.out.println("Total area: " + totalArea(myCircles));
 	}
 	
-	public static double totalArea(List<Shape> list){
+	public static double totalArea(List<? extends Shape> list){
 		double sum = 0.0;
 		for(Shape s : list) {
 			sum += s.area();
