d41d18e7aa396edc87571a798440bee665589f5e
zBragaTo <leonardobragatobassi@hotmail.com>
2024-06-28 21:53:07 -0300

    Organizing Code

--- src/CuringaDel/Program.java
+++ src/CuringaDel/Program.java
@@ -9,11 +9,9 @@ public class Program {
 		
 		List<Shape> myShapes = new ArrayList<>();
 		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
 		
 		List<Circle> myCircles = new ArrayList<>();
 		myCircles.add(new Circle(3.0));
-		myCircles.add(new Circle(2.0));
 		 
 		System.out.println("Total area: " + totalArea(myCircles));
 	}
