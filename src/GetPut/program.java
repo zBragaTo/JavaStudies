e58286935de14dfa2f1de902dbc4aadad2862936
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-03 20:48:01 -0300

    contravariancia

--- src/GetPut/program.java
+++ src/GetPut/program.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class program {
 	public static void main(String[] args) {
 		
-		List<Integer> intList = new ArrayList<Integer>();
+		/*List<Integer> intList = new ArrayList<Integer>();
 		intList.add(10);
 		intList.add(5);
 		
@@ -15,6 +15,20 @@ public class program {
 		Number x = list.get(0);
 		
 		list.add(20);
+		*/
+		
+		List<Object> MyObjs = new ArrayList<Object>();
+		MyObjs.add("maria");
+		MyObjs.add("alex");
+		
+		List<? super Number> myNums = MyObjs;
+		
+		myNums.add(10);
+		myNums.add(3.14);
+
+		Number x = myNums.get(0);
+				
+		
 	}
 
 }
