ff6d3e68d225e919b07996baf61fcb8fca4d8df8
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-03 20:40:59 -0300

    covariancia

--- src/GetPut/program.java
+++ src/GetPut/program.java
@@ -1,8 +1,20 @@
 package GetPut;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class program {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		
+		List<? extends Number> list = intList;
+		
+		Number x = list.get(0);
+		
+		list.add(20);
 	}
 
 }
