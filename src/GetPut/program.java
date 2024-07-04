3ef64974afe203adf0926eae19421a643a654319
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-03 21:20:01 -0300

    copy

--- src/GetPut/program.java
+++ src/GetPut/program.java
@@ -1,12 +1,14 @@
 package GetPut;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class program {
 	public static void main(String[] args) {
 		
-		/*List<Integer> intList = new ArrayList<Integer>();
+		/*
+		List<Integer> intList = new ArrayList<Integer>();
 		intList.add(10);
 		intList.add(5);
 		
@@ -17,6 +19,7 @@ public class program {
 		list.add(20);
 		*/
 		
+		/*
 		List<Object> MyObjs = new ArrayList<Object>();
 		MyObjs.add("maria");
 		MyObjs.add("alex");
@@ -27,8 +30,30 @@ public class program {
 		myNums.add(3.14);
 
 		Number x = myNums.get(0);
-				
+		*/
 		
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);		
+		printList(myObjs);
+		
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for(Number number : source) {
+			destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 
 }
