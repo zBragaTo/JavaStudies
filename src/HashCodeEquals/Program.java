af92402bace0659df421fbc22b79b285db6faf59
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-16 21:23:41 -0300

    HashCode

--- src/HashCodeEquals/Program.java
+++ src/HashCodeEquals/Program.java
@@ -4,10 +4,21 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		/* 
 		String a = "Maria";
 		String b = "Alex";
 		
 		System.out.println(a.equals(b));
+		*/
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+
+		
+		
 
 	}
 
