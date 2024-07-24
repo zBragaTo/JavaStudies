043fce9df8dfd3ca17e77fd78011a74f04e78f52
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-24 20:28:07 -0300

    LinkedHashSet

--- src/Set/Program.java
+++ src/Set/Program.java
@@ -1,19 +1,19 @@
 package Set;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
 		
 		set.add("Tv");
 		set.add("Notebook");
 		set.add("Tablet");
 		
-		System.out.println(set.contains("Notebook"));
+		set.removeIf(x -> x.length() >= 3);
 		
 		for(String p : set) {
 			System.out.println(p);
