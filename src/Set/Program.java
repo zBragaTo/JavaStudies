7750c96b8d7cf002e6678dd157a4dbd85ae82c4f
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-30 20:28:06 -0300

    Union Treeset

--- src/Set/Program.java
+++ src/Set/Program.java
@@ -1,22 +1,20 @@
 package Set;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new LinkedHashSet<>();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 		
-		set.add("Tv");
-		set.add("Notebook");
-		set.add("Tablet");
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
-		set.removeIf(x -> x.length() >= 3);
-		
-		for(String p : set) {
-			System.out.println(p);
 		}
 	}
-}
