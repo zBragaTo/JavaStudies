e327d31d5f0575b3d9062a745c998f11ce4b2be7
zBragaTo <leonardobragatobassi@hotmail.com>
2024-07-31 19:49:27 -0300

    Contains in hashcode

---
+++ src/Set/entities/Product.java
@@ -0,0 +1,50 @@
+package Set.entities;
+
+import java.util.Objects;
+
+public class Product {
+	private String product;
+	private double price;
+	
+	Product(String product, double price){
+		this.product = product;
+		this.price = price;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(price, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& Objects.equals(product, other.product);
+	}
+	
+	
+	
+}
