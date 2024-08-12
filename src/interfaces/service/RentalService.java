f7019705fc202a2f11bb000e41d3a37bb87148c0
zBragaTo <leonardobragatobassi@hotmail.com>
2024-08-12 19:25:42 -0300

    Finishing the code

--- src/interfaces/service/RentalService.java
+++ src/interfaces/service/RentalService.java
@@ -1,5 +1,7 @@
 package interfaces.service;
 
+import java.time.Duration;
+
 import interfaces.entities.CarRental;
 import interfaces.entities.Invoice;
 
@@ -17,7 +19,20 @@ public class RentalService {
 	}
 	
 	public void processInvoice(CarRental carRental) {
-		carRental.setInvoice(new Invoice(50.0, 10.0));
+		
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		} else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
 	
 }
